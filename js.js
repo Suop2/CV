3c54435a9585cc4c8621c91eabf877fa0232d9ab
Suop2 <57939454+Suop2@users.noreply.github.com>
2024-04-08 16:32:05 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click', function() {
+	alert('Смейся и весь мир будет смеятся вместе с тобой. Плачь и ты будешь плакать в одиночестве')
+})
